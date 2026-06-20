8ca6cad135857cdcf7ad246858ad86e97182a3f9
zjl <570633570@qq.com>
2026-06-20 20:43:42 +0800

    UI First

--- app/src/main/java/com/example/multiweather/logic/network/PlaceService.kt
+++ app/src/main/java/com/example/multiweather/logic/network/PlaceService.kt
@@ -9,4 +9,5 @@ import retrofit2.http.Query
 interface PlaceService {
     @GET("v2/place?token=${MultiWeatherApplication.TOKEN}&lang=zh_CN")
     suspend fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+
 }
